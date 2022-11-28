d2a9f3af46df3269e1ebc58b75ee1d2b5758a107
MarcinSzkurlat <szkurlat.martin@gmail.com>
2022-11-28 14:00:03 +0100

    rebase and add parameters in querys

--- data_manager.py
+++ data_manager.py
@@ -11,9 +11,9 @@ import database_common
 def get_all_question_data(cursor, table):
     query = f"""
             SELECT *
-            FROM {table}
+            FROM %s
             """
-    cursor.execute(query)
+    cursor.execute(query, [table])
     return cursor.fetchall()
 
 
@@ -21,9 +21,9 @@ def get_all_question_data(cursor, table):
 def add_new_question(cursor, submission_time, view_number, vote_number, title, message, image):
     query = f"""
                 INSERT INTO question (submission_time, view_number, vote_number, title, message, image)
-                VALUES ('{submission_time}', '{view_number}', '{vote_number}', '{title}', '{message}', {image})
+                VALUES (%s, %s, %s, %s, %s, %s)
                 """
-    cursor.execute(query)
+    cursor.execute(query, [submission_time, view_number, vote_number, title, message, image])
 
 
 @database_common.connection_handler
@@ -35,17 +35,17 @@ def delete_question(cursor, question_id):
         WHERE question_id = %s;
         DELETE FROM answer
         WHERE question_id = %s;"""
-    cursor.execute(query, [question_id])
+    cursor.execute(query, [question_id, question_id, question_id])
 
 
 @database_common.connection_handler
 def get_question_by_id(cursor, question_id):
-    query = f"""
+    query = """
                 SELECT *
                 FROM question
-                where id = '{question_id}'
+                where id = %s
                 """
-    cursor.execute(query)
+    cursor.execute(query, [question_id])
     return cursor.fetchall()
 
 @database_common.connection_handler
@@ -53,9 +53,9 @@ def get_question_id(cursor, time):
     query = f"""
                 SELECT id
                 FROM question
-                where submission_time = '{time}'
+                where submission_time = %s
                 """
-    cursor.execute(query)
+    cursor.execute(query, [time])
     return cursor.fetchall()
 
 
@@ -64,9 +64,9 @@ def get_answers_by_question_id(cursor, question_id):
     query = f"""
                     SELECT *
                     FROM answer
-                    where question_id = '{question_id}'
+                    where question_id = %s
                     """
-    cursor.execute(query)
+    cursor.execute(query, [question_id])
     return cursor.fetchall()
 
 

c77822844a254f5e1f2ec45ddd56dd5d008f774e
TomaszCitko <citkotomasz@gmail.com>
2022-12-14 20:29:27 +0100

    JS search working 3 from 4 requirements

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -1,8 +1,8 @@
 // you receive an array of objects which you must sort in the by the key "sortField" in the "sortDirection"
 function getSortedItems(items, sortField, sortDirection) {
-    console.log(items)
-    console.log(sortField)
-    console.log(sortDirection)
+    // console.log(items)
+    // console.log(sortField)
+    // console.log(sortDirection)
 
     // === SAMPLE CODE ===
     // if you have not changed the original html uncomment the code below to have an idea of the
@@ -25,18 +25,23 @@ function getSortedItems(items, sortField, sortDirection) {
 
 // you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
 function getFilteredItems(items, filterValue) {
-    console.log(items)
-    console.log(filterValue)
-
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-    //
-    for (let i=0; i<filterValue.length; i++) {
-        items.pop()
+    let filtered_items = []
+        for (let i=0; i<items.length; i++) {
+            if (items[i]['Title'].includes(filterValue)) {
+                filtered_items.push(items[i])
+            }
+            else if (filterValue[0] === "!" && !(items[i]['Title'].includes(filterValue.substring(1,filterValue.length)))) {
+                filtered_items.push(items[i])
+            }
+            else if (filterValue.substring(0,12) === "Description:" && (items[i]['Description'].includes(filterValue.substring(12,filterValue.length)))) {
+                filtered_items.push(items[i])
+            }
+            else if (filterValue.substring(0,13) === "!Description:" && !(items[i]['Description'].includes(filterValue.substring(13,filterValue.length)))) {
+                filtered_items.push(items[i])
+            }
     }
 
-    return items
+    return filtered_items
 }
 
 function toggleTheme() {
